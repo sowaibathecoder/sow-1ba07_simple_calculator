178cc07dbaf135e43bf34d0d74fe8b0c7389c43c
SOWAIBA NAZ <167256849+sowaibathecoder@users.noreply.github.com>
2024-09-13 07:36:06 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,31 +1,46 @@
 #! /usr/bin/env node
 
-import inquirer from "inquirer"
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(
+  chalk.bgMagentaBright("\n\t\t\t\t\t ****A SIMPLE CALCULATOR**** \n\t\t\t\t\t")
+);
 
 const result = await inquirer.prompt([
-  { message: "Enter your first number:", type: "number", name: "firstNumber" },
-  { message: "Enter your second number:", type: "number", name: "secondNumber" },
   {
-    message: "Select one of the operators to perform operation:",
+    message: chalk.yellowBright("Enter your first number:"),
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: chalk.yellowBright("Enter your second number:"),
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: chalk.yellowBright(
+      "Select one of the operators to perform operation:"
+    ),
     type: "list",
     name: "operators",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    choices: [
+      chalk.magenta("Addition"),
+      chalk.green("Subtraction"),
+      chalk.yellow("Multiplication"),
+      chalk.red("Division"),
+    ],
   },
 ]);
 
-
-if (result.operators === "Addition"){
-    console.log(result.firstNumber + result.secondNumber);
-}
-else if (result.operators === "Subtraction"){
-    console.log(result.firstNumber - result.secondNumber);
-}
-else if (result.operators === "Multiplication"){
-    console.log(result.firstNumber * result.secondNumber);
-}
-else if (result.operators === "Division"){
-    console.log(result.firstNumber / result.secondNumber);
-}
-else{
-    console.log("Please select valid operator");
+if (result.operators === chalk.magenta("Addition")) {
+  console.log(result.firstNumber + result.secondNumber);
+} else if (result.operators === chalk.green("Subtraction")) {
+  console.log(result.firstNumber - result.secondNumber);
+} else if (result.operators === chalk.yellow("Multiplication")) {
+  console.log(result.firstNumber * result.secondNumber);
+} else if (result.operators === chalk.red("Division")) {
+  console.log(result.firstNumber / result.secondNumber);
+} else {
+  console.log(chalk.red("Please select valid operator"));
 }
