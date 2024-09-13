967db3458b236f93ccd852d1a40cc887f4413b8c
SOWAIBA NAZ <167256849+sowaibathecoder@users.noreply.github.com>
2024-09-13 07:37:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgMagentaBright("\n\t\t\t\t\t ****A SIMPLE CALCULATOR**** \n\t\t\t\t\t"));
 const result = await inquirer.prompt([
-    { message: "Enter your first number:", type: "number", name: "firstNumber" },
-    { message: "Enter your second number:", type: "number", name: "secondNumber" },
     {
-        message: "Select one of the operators to perform operation:",
+        message: chalk.yellowBright("Enter your first number:"),
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: chalk.yellowBright("Enter your second number:"),
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: chalk.yellowBright("Select one of the operators to perform operation:"),
         type: "list",
         name: "operators",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        choices: [
+            chalk.magenta("Addition"),
+            chalk.green("Subtraction"),
+            chalk.yellow("Multiplication"),
+            chalk.red("Division"),
+        ],
     },
 ]);
-if (result.operators === "Addition") {
+if (result.operators === chalk.magenta("Addition")) {
     console.log(result.firstNumber + result.secondNumber);
 }
-else if (result.operators === "Subtraction") {
+else if (result.operators === chalk.green("Subtraction")) {
     console.log(result.firstNumber - result.secondNumber);
 }
-else if (result.operators === "Multiplication") {
+else if (result.operators === chalk.yellow("Multiplication")) {
     console.log(result.firstNumber * result.secondNumber);
 }
-else if (result.operators === "Division") {
+else if (result.operators === chalk.red("Division")) {
     console.log(result.firstNumber / result.secondNumber);
 }
 else {
-    console.log("Please select valid operator");
+    console.log(chalk.red("Please select valid operator"));
 }
